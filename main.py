30392f377fb35c243a6c0b952fa55f08059f7501
Dan11lkaa <bayd3386@gmail.com>
2024-08-03 00:25:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from aiogram import Bot, Dispatcher
 import asyncio
 from config import TOKEN
-from app.handlers import router
+from handlers import router
 from aiogram.types import BotCommandScopeDefault
-from app.keyboard import commands
-from app.database.models import async_main
+from keyboard import commands
+from models import async_main
 async def main():
     await async_main()
     bot = Bot(token=TOKEN)
